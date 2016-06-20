d64072cd15a09cba31d746f8a9fb9c3f0305277f
kimseongyun <fri1121@naver.com>
2016-06-20 23:42:05 +0900

    Add files via upload

--- Journey.java
+++ Journey.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
 public class Journey{
+ 
   public static void main(String[] args){
     System.out.println("The man has 100Zen points.");
     
+    int hour ;
+    Scanner s =  new Scanner(System.in);
+    
+    System.out.println("Write your meditating hour");
+    hour = s.nextInt();
+    
+    if  (hour > 2);
+      System.out.println("You got 50 worked.");
+      
+    package com.software.design.problem.call;
+    public class ChilderenClass extends ParentClass {
+      private String name;
+      public ChlidrenClass(){
+        super();
+        name = "I'm a Monk.";
+      }
+      @Override
+      public String toString(){
+        return name.toUpperCase();
+      
+    
+  }
+}
   }
 }
